a74c641358a1945c1bd2b180812627ccf2ed83bf
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-10 21:56:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 // Code your solution here!
 
+/* function printString(myString) {
+  console.log(myString[0]);
+ 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(myString) {
+     console.log(myString[(myString.length-1)]);
+  
+  
+   if (myString.length >1 ) {
+    let mySubString = myString.substring(0, myString.length-1);
+    reverseString(mySubString);
+    
+  } else {
+    return true;
+  }
+}
+*/
+function isPalindrome(myString) {
+  console.log (myString);
+  if (myString.length % 2 === 0) {
+     if (myString[0] === myString[myString.length-1] && myString.length >2 ) {
+       let mySubString = myString.substring(1, myString.length-2);
+       isPalindrome (mySubString);
+     } else {
+       return false;
+     }
+     }
+  } else if (myString.length % 2 === 1); {
+      if (myString[0] === myString[myString.length-1] && myString.length >1 ) {
+       let mySubString = myString.substring(1, myString.length-2);
+       isPalindrome (mySubString);
+      } else {
+        return false;
+      }
+  } else 
+  
+  isPalindrome("abccba");
