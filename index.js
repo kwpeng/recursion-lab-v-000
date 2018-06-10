7f9e79b5bab751af67350000fd1e6468e48e2001
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-10 23:17:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function isPalindrome(myString) {
        let mySubString = myString.substring(1, myString.length-2);
        isPalindrome (mySubString);
      } else {
-       return false;
+       return true;
      }
   
   } else if (myString.length % 2 === 1) {
@@ -37,7 +37,7 @@ function isPalindrome(myString) {
        let mySubString = myString.substring(1, myString.length-2);
        isPalindrome (mySubString);
       } else {
-        return false;
+        return true;
       }
   } else {
   return false;
